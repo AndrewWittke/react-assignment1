c8f5f4c9e6e8dc30ca85549851103996bf9155f9
andrewwittke <awittke@calpoly.edu>
2021-01-08 16:02:29 -0800

    most steps complete

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 function TableHeader() {
   return (
@@ -14,10 +14,17 @@ function TableHeader() {
 function TableBody (props) {
   const rows = props.characterData.map((row, index) => {
     return (
-      <tr key={index}>
-        <td>{row.name}</td>
-        <td>{row.job}</td>
-      </tr>
+      
+            
+            <tr key={index}>
+              <td>{row.name}</td>
+              <td>{row.job}</td>
+              <td>
+                <button onClick={() => props.removeCharacter(index)}>Delete</button>
+              </td>
+            </tr>
+
+
     );
   });
   return (
@@ -27,12 +34,16 @@ function TableBody (props) {
    );
 }
 
+
+
 function Table(props) {
-   return(
-      <table>
-        <TableHeader/>
-        <TableBody characterData={props.characterData} />
-      </table>
-   );
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
+
+
 export default Table;
